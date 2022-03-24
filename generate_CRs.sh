358b43dcbad7eb6a76b053a8b004467a7bec3dcd
sreckoj <srecko.janjic@gmail.com>
2022-03-24 19:43:34 +0100

    Update generate_CRs.sh

--- generate_CRs.sh
+++ generate_CRs.sh
@@ -3,6 +3,7 @@ echo "---------------------------------"
 whoami
 echo "---------------------------------"
 
+
 PathToConfigFolder=/workspace/output/initial-config
 
 DIRbarauth=${PathToConfigFolder}/barauth
@@ -19,7 +20,7 @@ CRs_generated_folder=/workspace/output/operator_resources_CRs/generated
 
 BARurl=${NEXUS_SERVER_BASE_URL}/repository/${NEXUS_REPOSITORY_NAME}/${NEXUS_PATH}/${BAR_VERSION}/${BAR_NAME}-${BAR_VERSION}.bar
 
-mkdir ${CRs_generated_folder}
+su -c mkdir ${CRs_generated_folder}
 mkdir ${CRs_generated_folder}/configurations
 
 # Create the Integration Server CR in any case
